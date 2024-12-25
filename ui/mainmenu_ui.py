7234b943d3ddccb2622b957942e7397e38be6f6e
xbinh <binhnguyenxuan47@gmail.com>
2024-12-25 15:39:24 +0700

    fix

--- ui/mainmenu_ui.py
+++ ui/mainmenu_ui.py
@@ -535,7 +535,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(2)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
